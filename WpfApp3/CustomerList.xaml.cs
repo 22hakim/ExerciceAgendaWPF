3fc001fec9ef0eb85be9159112daf02f58b34147
hakim22 <hakim_yahi@hotmail.fr>
2022-02-28 16:35:34 +0100

    display customer details in aside

--- WpfApp3/CustomerList.xaml.cs
+++ WpfApp3/CustomerList.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using WpfApp3.Data;
 using WpfApp3.Models;
 
@@ -21,6 +23,17 @@ namespace WpfApp3
             this.DataContext = new Customer();
             List<Customer> customerList = _db.Customers.ToList();
             customerDataGrid.ItemsSource = customerList;
+        }
+
+        private void displayCustomer(object sender, RoutedEventArgs e)
+        {
+            Customer customer_row = (Customer)customerDataGrid.SelectedItem;
+            lastnameInput.Text = customer_row.Lastname;
+            firstnameInput.Text = customer_row.Firstname;
+            mailInput.Text = customer_row.Mail;
+            phoneInput.Text = customer_row.PhoneNumber;
+            budgetInput.Text = customer_row.Budget.ToString();
+            id.Content = customer_row.IdCustomer;
         }
 
         private void updateCustomer(object sender, System.Windows.RoutedEventArgs e)
