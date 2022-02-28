8965464ab3b02439a16ada7e76bdb38e04e17544
hakim22 <hakim_yahi@hotmail.fr>
2022-02-28 16:52:00 +0100

    update customer

--- WpfApp3/CustomerList.xaml.cs
+++ WpfApp3/CustomerList.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using WpfApp3.Data;
 using WpfApp3.Models;
 
@@ -14,6 +16,8 @@ namespace WpfApp3
     /// </summary>
     public partial class CustomerList : Page
     {
+        public List<bool> errorList = new List<bool>();
+
         private readonly agendaContext _db;
         public CustomerList()
         {
@@ -38,6 +42,24 @@ namespace WpfApp3
 
         private void updateCustomer(object sender, System.Windows.RoutedEventArgs e)
         {
+            Customer newCustomer = _db.Customers.Find(Convert.ToInt32(id.Content));
+            newCustomer.Firstname = CheckString(firstnameInput.Text, "prénom");
+            newCustomer.Lastname = CheckString(lastnameInput.Text, "nom");
+            newCustomer.Budget = CheckBudget(budgetInput.Text, "budget");
+            newCustomer.PhoneNumber = CheckTelephoneNumber(phoneInput.Text, "numéro de telephone");
+            newCustomer.Mail = mailInput.Text;
+
+
+            if (errorList.Contains(false))
+            {
+                errorList = new List<bool>();
+                return;
+            }
+
+            _db.Customers.Update(newCustomer);
+            _db.SaveChanges();
+
+            this.NavigationService.Refresh();
 
         }
 
@@ -45,5 +67,54 @@ namespace WpfApp3
         {
 
         }
+        public string CheckString(string value, string name)
+        {
+            //Regex regex = new Regex(@"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$");
+            //Match match = regex.Match(value.ToString());
+            if (value == null || value == String.Empty)
+            {
+                addTextBox(name);
+                errorList.Add(false);
+            }
+
+            return value;
+
+        }
+
+        public string CheckTelephoneNumber(string value, string name)
+        {
+            Regex regex = new Regex(@"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$");
+            Match match = regex.Match(value.ToString());
+            if (value == String.Empty || match == Match.Empty)
+            {
+                addTextBox(name);
+                errorList.Add(false);
+            }
+            return value;
+        }
+
+        public int CheckBudget(object value, string name)
+        {
+            int myInt;
+            if (int.TryParse((string?)value, out myInt))
+            {
+                return myInt;
+            }
+            else
+            {
+                addTextBox(name);
+                errorList.Add(false);
+                return 0;
+            }
+        }
+
+        private void addTextBox(string value)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Le champs " + value + " n'est pas valide";
+            textBlock.Foreground = new SolidColorBrush(Colors.Red);
+            errorForm.Children.Add(textBlock);
+        }
+
     }
 }
